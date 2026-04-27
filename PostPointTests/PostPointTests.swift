f7c9b7877974984124ddeb29d6c00fbb873a0b4e
forant <tforan@gmail.com>
2026-04-27 11:07:17 -0700

    Initial Commit

---
+++ PostPointTests/PostPointTests.swift
@@ -0,0 +1,19 @@
+//
+//  PostPointTests.swift
+//  PostPointTests
+//
+//  Created by Tim Foran on 4/27/26.
+//
+
+import Testing
+@testable import PostPoint
+
+struct PostPointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
